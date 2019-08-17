4602ca880eed2bb66c174a35a2f5f15f8cdb7d44
Kia "Xi4" Armani <contact@xi4.me>
2019-08-17 15:48:22 +0000

    Initial commit

---
+++ System/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace microservice_template.Controllers
+{
+    [Route("system/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // GET system/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok();
+        }
+    }
+}
